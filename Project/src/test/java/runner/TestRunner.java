f24170bd34ef6556514b6ca53dbe74009d2dd7a1
tapaswinipsnist <tapaswinip1804@gmail.com>
2025-04-25 17:16:43 +0000

    bdd commit-02 tapaswini

--- Project/src/test/java/runner/TestRunner.java
+++ Project/src/test/java/runner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="features",glue="stepdefinition")
+@CucumberOptions(features="features",glue="stepdefinition",plugin = {"pretty"})
 public class TestRunner {
 
 }
--- Project/src/test/java/stepdefinition/Hooks.java
+++ Project/src/test/java/stepdefinition/Hooks.java
@@ -1,25 +1,21 @@
 package stepdefinition;
-import org.junit.AfterClass;
 import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-
 import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
 import io.cucumber.java.BeforeAll;
-import io.cucumber.java.AfterAll;
-import io.cucumber.java.Scenario;
 import utils.Base;
 import utils.Emails;
 import utils.Reporter;
 public class Hooks extends Base{
     static ExtentReports reports;
-    static ExtentTest test;
     @BeforeAll
     public static void createReport(){
+        System.out.println("Starting Report");
         reports = Reporter.generateTestReport("Reliance_Report");
     }
     @Before
-    public void openReliance(Scenario scenario){
+    public void openReliance(){
         openBrowser();
     }
     @After
@@ -28,9 +24,8 @@ public class Hooks extends Base{
     }
     @AfterAll
     public static void closeReport(){
+        System.out.println("Flushing report");
         reports.flush();
         Emails.sendEmail();
     }
 }
- 
- 
